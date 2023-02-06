5846acc16155c77519cdad4f48cccd21ecfb4b49
Nazky <56560636+Nazky@users.noreply.github.com>
2023-02-06 11:07:13 +0100

    first commit

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+function sa(){
+    document.getElementById('tabloa').style.display = 'block';
+    document.getElementById('tabloe').style.display = 'none';
+    document.getElementById('tablog').style.display = 'none';
+    document.getElementById('tabloc').style.display = 'none';
+}
+
+function se(){
+    document.getElementById('tabloa').style.display = 'none';
+    document.getElementById('tabloe').style.display = 'block';
+    document.getElementById('tablog').style.display = 'none';
+    document.getElementById('tabloc').style.display = 'none';
+}
+
+function sg(){
+    document.getElementById('tabloa').style.display = 'none';
+    document.getElementById('tabloe').style.display = 'none';
+    document.getElementById('tablog').style.display = 'block';
+    document.getElementById('tabloc').style.display = 'none';
+}
+
+function sc(){
+    document.getElementById('tabloc').style.display = 'block';
+    document.getElementById('tabloa').style.display = 'none';
+    document.getElementById('tabloe').style.display = 'none';
+    document.getElementById('tablog').style.display = 'none';
+}
